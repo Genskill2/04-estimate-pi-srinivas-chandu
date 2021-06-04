1966832e0c13973aadec9559724e771db47fa9e0
srinivas-chandu <srinivaschandubojjagani@gmail.com>
2021-06-04 09:51:58 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -31,3 +31,18 @@ float wallis_pi(int count){
     }
     return pi * 2;
 } 
+float mc_pi(int n ){
+   int incircle = 0;
+   
+  for ( int i=0; i<n ;i++ ){
+  float x = frandom();
+  float y= frandom();
+  float  d= x*x + y*y;
+  if ( d <= 1 ){
+   incircle++;
+   }
+  }
+  float pi = 4*(float)incircle/n;
+   return pi;   
+  
+  }
