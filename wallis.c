05fb805d410f223024334a10d3c9657498289e60
srinivas-chandu <srinivaschandubojjagani@gmail.com>
2021-06-04 09:58:11 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,26 +23,3 @@ int main(void) {
     }
   }
 }
-float mc_pi(int n ){
-   int incircle = 0;
-   
-  for ( int i=0; i<n ;i++ ){
-  float x = frandom();
-  float y= frandom();
-  float  d= x*x + y*y;
-  if ( d <= 1 ){
-   incircle++;
-   }
-  }
-  float pi = 4*(float)incircle/n;
-   return pi;   
-  
-  }
-float wallis_pi(int count){
-    float pi = 1.0;
-    for (int i = 1; i < count; i++){
-        float num = 4.0 * i * i;
-        pi *= num / (num - 1);
-    }
-    return pi * 2;
-} 
